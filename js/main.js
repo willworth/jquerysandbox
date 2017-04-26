97d337299272eeeaf8f19068b6b6b52f169a52b5
will <code@willworth.co.uk>
2017-04-26 10:25:22 +0200

    add css id example

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//basic js test
 console.log("Js coming through...");
 
-$("h1").text("Here is my modified h1 because jquery works!");
+// always wait for the page!
+$(document).ready(function(){
+    $("h1").text("Here is my modified h1 because jquery works!");
+});
+
+
+// $("span").text("$100");
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-This is a sandbox for basic jquery manipulation.
+#This is a sandbox for basic jquery manipulation.
+
+##Notes
+It's important to wait for dom to finish loading.
+
+$(document).ready(function(){
+    //whatever
+});
